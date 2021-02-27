a023495a125b2ffc798e517e61dca952b339ae01
Nitinpal269 <76909634+Nitinpal269@users.noreply.github.com>
2021-02-27 22:28:42 +0530

    mycharwriter

---
+++ MyCharWriter.java
@@ -0,0 +1,20 @@
+import java.io.*;
+class MyCharWriter
+{
+public static void main(String[] args)throws IOException
+{
+CharArrayWriter fw=new CharArrayWriter();
+String s="india is good country sss";
+char ch[] =s.toCharArray();
+//for(int i=0; i<ch.length; i++)
+fw.write(s);
+//fw.write(ch);
+fw.writeTo(new FileWriter("abc1.txt"));
+System.out.println(fw.toString());
+char z[]=fw.toCharArray();
+CharArrayReader fr=new CharArrayReader(z);
+int i=0;
+while((i=fr.read())!=-1)
+System.out.println((char)i);
+}
+}
