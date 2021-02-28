7d586452f775e647b5b4b61dd21bbd8e5817ebf5
Nitinpal269 <76909634+Nitinpal269@users.noreply.github.com>
2021-02-28 23:26:07 +0530

    file

---
+++ CWD.java
@@ -0,0 +1,9 @@
+// this is use for show the path of file saved in folder
+public class CWD
+{
+public static void main(String[] args)
+{
+String curDir=System.getProperty("user.dir");
+System.out.println(curDir);
+}
+}
