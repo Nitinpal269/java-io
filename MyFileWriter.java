a1777a76c278bbefe2750c528c96b5398d267c42
Nitinpal269 <76909634+Nitinpal269@users.noreply.github.com>
2021-03-03 23:35:45 +0530

    file read write stream

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -3,20 +3,14 @@ class MyFileWriter
 {
 public static void main(String[] args)throws IOException
 {
-//FileOutputStream fw=new FileOutputStream("lalu.txt");//Write Mode
-//FileOutputStream fw=new FileOutputStream("lalu.txt", true);//Append Mode
-//FileOutputStream fw=new FileOutputStream(new File("D:\\program\\Input Output\\Byte Stream\\FileInputOutputStream\\lalu2.txt"));
-FileOutputStream fw=new FileOutputStream(new File("D:\\program\\Input Output\\Byte Stream\\FileInputOutputStream\\lalu3.txt"),true);
-String s="India is a good country";
-
-
-
-byte ch[]=s.getBytes();
-fw.write(ch);
+FileWriter fw=new FileWriter("ducat11.txt");
+//FileWriter fw=new FileWriter("ducat11.txt",true);
+//FileWriter fw=new FileWriter(new File("D:\program\Input Output\Character Stream\FileReaderWriterStream"),true);
+String s="india is good country ssss";
+char ch[]=s.toCharArray();
 for(int i=0; i<ch.length; i++)
-//fw.write(ch[i]);
+fw.write(ch[i]);//fw.write(ch) or fw.write(s)
 fw.close();
-System.out.println("File created successfully");
 }
 }
 
@@ -24,23 +18,10 @@ class MyFileReader
 {
 public static void main(String[] args)throws IOException
 {
-FileInputStream fr=new FileInputStream("lalu.txt");
-//FileInputStream fr=new FileInputStream(new File("D:\\program\\Input Output\\FileInputOutputStream\\lalu2.txt"));
+FileReader fr=new FileReader("ducat11.txt");
 int i=0;
 while((i=fr.read())!=-1)
 System.out.print((char)i);
 fr.close();
-}
-}
-
-class MyFileReader1
-{
-public static void main(String[] args)throws IOException
-{
-FileInputStream fr=new FileInputStream("lalu.txt");
-byte b[]=new byte[fr.available()];
-fr.read(b);
-String s=new String(b);
-System.out.println(s);
 }
 }
